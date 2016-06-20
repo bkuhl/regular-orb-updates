abdd28402f16d35bf4bddf903410c7e6d03a465d
Ben Kuhl <ben.kuhl@realpage.com>
2016-06-20 09:00:00 -0400

    Only push updates on weekdays

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(DailyUpdates::SIGNATURE)->dailyAt('9am');
+        $schedule->command(DailyUpdates::SIGNATURE)->weekdays()->at('9am');
     }
 }
